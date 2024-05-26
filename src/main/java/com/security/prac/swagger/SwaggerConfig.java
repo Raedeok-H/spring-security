164df031b8410fcdec7e8eb56f35c514ecccd518
Raedeok-H <raedeokhwang@gmail.com>
2024-05-26 13:34:20 +0900

    swagger 문서 내 jwt 세팅 설정

--- src/main/java/com/security/prac/swagger/SwaggerConfig.java
+++ src/main/java/com/security/prac/swagger/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.security.prac.swagger;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,6 +12,24 @@ import org.springframework.context.annotation.Configuration;
 // swagger-ui.html 경로로 접근 가능
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI api() {
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityRequirement)
+                .info(getInfo());
+    }
 
     // TODO: 로그인과 게시판 기능이 나오면 하위 엔드포인트로 분리할 것
     @Bean
@@ -21,9 +41,11 @@ public class SwaggerConfig {
     }
 
     @Bean
-    public OpenAPI getOpenApi() {
-        return new OpenAPI().components(new Components())
-                .info(getInfo());
+    public GroupedOpenApi testApi() {
+        return GroupedOpenApi.builder()
+                .group("TEST")
+                .pathsToMatch("/test/**")
+                .build();
     }
 
     private Info getInfo() {
